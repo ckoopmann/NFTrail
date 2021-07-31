ebec2d6d36031a22312fe95057fb0adb54bc4cd6
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-07-31 13:17:45 +0200

    Remove alerts

--- dapp/src/components/TheHeader.vue
+++ dapp/src/components/TheHeader.vue
@@ -12,24 +12,7 @@
           />
         </div>
       </router-link>
-
-
     </v-app-bar>
-    <div v-if="walletConnected">
-      <v-alert v-if="contractDeployed" color="green" class="mb-0" dense>
-        Succesfully connected to the contract (<a :href="etherScanLink"
-          >etherscan</a
-        >).
-      </v-alert>
-      <v-alert v-else color="red lighten-2" class="mb-0" dense>
-        Contract is not deployed on {{ networkType }} network please switch your
-        network to the Rinkeby Test-Network.
-      </v-alert>
-    </div>
-    <v-alert v-else color="red lighten-2" class="mb-0" dense>
-        You do not seem to have Metamask connected to this site. To use this application
-        you will have to <a href="https://metamask.io/download">install Metamask</a> and reload the page.
-    </v-alert>
   </div>
 </template>
 
