ba404f7d125b2c2505c0da58cb90be2821a2239f
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-08-01 18:56:44 +0200

    Prepare routes for separate app view

--- dapp/src/router/index.js
+++ dapp/src/router/index.js
@@ -1,14 +1,18 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import LandingPage from "../LandingPage.vue";
+import Home from "../views/Home.vue"
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "landing",
+    component: LandingPage,
+      children: [
+        { path: "", name: "home", component: Home },
+      ],
   },
 ];
 
