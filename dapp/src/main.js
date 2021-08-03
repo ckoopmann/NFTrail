be7402009120a332936d1c366f560e41f8a36405
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-08-03 11:40:51 +0200

    Remove PWA / worker registration

--- dapp/src/main.js
+++ dapp/src/main.js
@@ -1,10 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
-import "./registerServiceWorker";
 import i18n from './i18n'
 
 Vue.config.productionTip = false;
