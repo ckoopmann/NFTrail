64ba50e493b9ebc213d377495eea1b6946907eeb
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-08-08 11:33:43 +0200

    Factor out token summary card

--- dapp/src/views/TokenDetails.vue
+++ dapp/src/views/TokenDetails.vue
@@ -1,27 +1,10 @@
 <template>
-  <v-card class="mx-auto my-12" max-width="500">
-    <v-img max-width="500" :src="tokenData.pictureURI"></v-img>
-    <v-card-title>{{ tokenData.assetIdentifier }}</v-card-title>
-    <v-card-text>
-      <v-list-item>
-        <v-list-item-title>Token Id</v-list-item-title>
-        <v-list-item-subtitle>{{ tokenId }}</v-list-item-subtitle>
-      </v-list-item>
-    </v-card-text>
-  </v-card>
+  <TokenSummaryCard />
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import TokenSummaryCard from "../components/TokenSummaryCard.vue";
 export default {
-  computed: {
-    ...mapGetters("contractModule", ["ownedIds"]),
-    tokenId() {
-      return this.$route.params.id;
-    },
-    tokenData() {
-      return this.ownedIds[this.tokenId];
-    },
-  },
+  components: { TokenSummaryCard },
 };
 </script>
