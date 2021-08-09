1a0a8083ef39143af3c7b26959f2c548c2d64405
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-08-09 20:14:54 +0200

    Fix conditional hiding of document upload

--- dapp/src/views/TokenDetails.vue
+++ dapp/src/views/TokenDetails.vue
@@ -7,18 +7,28 @@
 </template>
 
 <script>
+import { ethers } from "ethers";
+import { mapActions, mapGetters } from "vuex";
+
 import TokenSummaryCard from "../components/TokenSummaryCard.vue";
 import UploadDocumentCard from "../components/UploadDocumentCard.vue";
 import DocumentListCard from "../components/DocumentListCard.vue";
-import { mapActions, mapGetters } from "vuex";
 export default {
   components: { TokenSummaryCard, UploadDocumentCard, DocumentListCard },
   computed: {
-    ...mapGetters("contractModule", "currentTokenDetails"),
-    ...mapGetters("web3Module", "selectedAccount"),
+    ...mapGetters("contractModule",["currentTokenDetails"]),
+    ...mapGetters("web3Module",["selectedAccount"]),
     isOwner() {
       if (this.currentTokenDetails == null) return false;
-      return this.currentTokenDetails.owner === this.selectedAccount;
+      console.log(
+        "Tokenowners",
+        this.currentTokenDetails.owner,
+        this.selectedAccount
+      );
+      return (
+        ethers.utils.getAddress(this.currentTokenDetails.owner) ===
+        ethers.utils.getAddress(this.selectedAccount)
+      );
     },
   },
   methods: mapActions("contractModule", ["loadTokenDetails"]),
